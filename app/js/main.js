6a16b53db27d057ec3195783b49c6517bd2823fd
MeztakeQ <gameholin123@mail.ru>
2023-06-12 22:21:34 +0300

    project3

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,173 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tlLoader = gsap.timeline()
+
+
+tlLoader
+  .set('.loader__item', {yPercent: -100})
+  .set('.loader__title', {opacity: 0})
+  .to('.loader__item', {
+    yPercent: 0,
+    duration: 0.5,
+    stagger: 0.25
+  })
+  .to('.loader__item', {
+    yPercent: 100,
+    duration: 0.5,
+    stagger: 0.25
+  })
+  .to('.loader__title', {
+    opacity: 1,
+    duration: 1,
+    scale: 1.2,
+  })
+  .set('.loader__item', {
+    yPercent: -100,
+  })
+  .to('.loader__title', {
+    opacity: 0,
+    duration: 1,
+    scale: 0.8,
+  })
+  .to('.loader', {
+   yPercent: -100,
+   duration: 1,
+  },
+  '-=0.2',
+  )
+
+
+//Laptop Animations
+
+
+
+  gsap.to('.home__content ', {
+    scrollTrigger: {
+      trigger: '.home',
+      start: '-100 0',
+      scrub: true,
+    },
+    yPercent: -100,
+  })
+
+  gsap.to('.home__picture-img ', {
+    scrollTrigger: {
+      trigger: '.home',
+      start: '-100 0',
+      scrub: true,
+    },
+    yPercent: 10,
+  })
+
+ //Section aboutme
+
+ gsap.from('.aboutme__title-slagl ', {
+  scrollTrigger: {
+    trigger: '.aboutme',
+    start: '-300 0',
+  },
+  x: 100,
+  autoAlpha: 0,
+  duration: 2,
+})
+
+gsap.from('.aboutme__title-slagr ', {
+  scrollTrigger: {
+    trigger: '.aboutme',
+    start: '-500 0',
+  },
+  x: 300,
+  autoAlpha: 0,
+  duration: 3,
+})
+
+// Section banner
+const tlBanner = gsap.timeline()
+
+tlBanner
+  .from('.banner', {
+    scrollTrigger: {
+      trigger: '.banner',
+      start: '-200 0',
+      scrub: true,
+      pin: true,
+    },
+  })
+
+.from('.banner__content-title', {
+  scrollTrigger: {
+    trigger: '.banner',
+    start: '-200 0',
+    scrub: true,
+  },
+  xPercent: -500,
+})
+
+.from('.banner__content-text', {
+  scrollTrigger: {
+    trigger: '.banner',
+    start: '-200  0',
+    scrub: true,
+  },
+  xPercent: 500,
+})
+
+//Section Project
+
+gsap.from('.project__item', {
+  scrollTrigger: {
+    trigger: '.project',
+    start: '-30% 0',
+    end: '-300px',
+  },
+  scale: 0,
+  stagger: 1,
+  duration: 0.5,
+})
+
+//Section Blog
+
+gsap.from('.blog__top-title', {
+  scrollTrigger: {
+    trigger: '.blog__inner ',
+    start: '-500 0',
+    end: '-100',
+    
+  },
+  opacity: 0,
+  autoAlpha: 0,
+  duration: 3,
+})
+
+gsap.from('.blog__item', {
+  scrollTrigger: {
+    trigger: '.blog__inner ',
+    start: '-300 0',
+    end: '-100',
+  },
+  opacity: 0,
+  stagger: 1,
+  duration: 0.5,
+  ease: 'bone'
+})
+
+// Section Contact
+
+gsap.from('.contact__top-title', {
+  scrollTrigger: {
+    trigger: '.contact ',
+    start: '-300 0',
+    end: '-100',
+    
+  },
+  opacity: 0,
+  duration: 0.5,
+  autoAlpha: 0,
+  duration: 3,
+})
+
+
+
 
 const cursor = document.querySelectorAll(".cursor");
 const links = document.querySelectorAll(".link");
@@ -22,22 +192,59 @@ window.addEventListener("mousemove", (e) => {
     
   })
   
-})   
+})  
 
 
-$(window).on('mousewheel DOMMouseScroll', function(e) {
-  var dir,
-      amt = 200;
+// const tl = gsap.timeline()
 
-  e.preventDefault();
-  if(e.type === 'mousewheel') {
-    dir = e.originalEvent.wheelDelta > 0 ? '-=' : '+=';
-  }
-  else {
-    dir = e.originalEvent.detail < 0 ? '-=' : '+=';
-  }      
+// tl.fromTo('.home__content-title', {
+//   x: -100,
+//   opacity: 0
+// }, {
+//   x: 0,
+//   opacity: 1,
+//   duration: 1
+// }).fromTo('.home__content-slagl', {
+//   y:50,
+//   opacity: 0,
+// }, {
+//   y: 0,
+//   opacity: 1,
+//   duration: 2,
+// }).fromTo('.home__content-slagr', {
+//   y:50,
+//   opacity: 0,
+// }, {
+//   y: 0,
+//   opacity: 1,
+//   duration: 3,
+// }).fromTo('.header__logo', {
+//   y:-150,
+//   opacity: 0,
+// }, {
+//   y: 0,
+//   opacity: 1,
+//   duration: 3,
+// },
+// .2
+// ).fromTo('.menu li', {
+//   y:-150,
+//   opacity: 0,
+// }, {
+//   y: 0,
+//   opacity: 1,
+//   duration: 1,
+//   stagger: 0.15,
+// },
+// .3
+// )
 
-  $('html, body').stop().animate({
-    scrollTop: dir + amt
-  },500, 'linear');
-})
+
+
+
+
+
+
+
+
+
